ed2765c5be576252451393792c66c721b049059d
Pablo Márquez <pablomarquezartesano@gmail.com>
2023-07-27 12:27:18 +0200

    Initial Commit

---
+++ RyMApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RyMApp
+//
+//  Created by Pablo Márquez Marín on 27/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
