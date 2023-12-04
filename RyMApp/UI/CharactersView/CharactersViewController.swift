61937398088f090570a2c4893cbbeef8fe95c72e
Pablo Márquez <pablomarquezartesano@gmail.com>
2023-12-04 14:07:23 +0100

    Characters actualizado

--- RyMApp/UI/CharactersView/CharactersViewController.swift
+++ RyMApp/UI/CharactersView/CharactersViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import Kingfisher
 
 class CharactersViewController: UIViewController {
-    
+    //MARK: - IBOutlets -
     @IBOutlet weak var characterBar: UITabBar!
     @IBOutlet weak var backImage: UIImageView!
     @IBOutlet weak var collectionCharacters: UICollectionView!
@@ -18,9 +18,11 @@ class CharactersViewController: UIViewController {
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
     
+    // MARK: - Propiedades -
     var model: AllCharacters
     var countPage = 1
     
+    // MARK: - Init -
     init(_ model: AllCharacters) {
         self.model = model
         super.init(nibName: nil,
@@ -31,10 +33,11 @@ class CharactersViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Ciclo de vida -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(named: "dark")
-        
         self.navigationController?.navigationBar.tintColor = UIColor(named: "rickHair")
         navigationItem.title = "Personajes"
         let textAttributes = [NSAttributedString.Key.foregroundColor: UIColor(named: "rickHair")]
@@ -63,12 +66,11 @@ class CharactersViewController: UIViewController {
         pagesLabel.text = "\(countPage) / \(model.info?.pages ?? 1)"
         pagesLabel.textColor = UIColor(named: "rickHair")
         pagesLabel.font = UIFont(name: "Get Schwifty Regular", size: 24)
-        
     }
     
-    // Botones
+    // MARK: - Botones -
     @IBAction func nextButtonAction(_ sender: Any) {
-        NetworkApi.shared.pages(url: (model.info?.next)!) { allCharacters in
+       /* NetworkApi.shared.pages(url: (model.info?.next)!) { allCharacters in
             self.model = allCharacters
             self.collectionCharacters.reloadData()
             self.countPage += 1
@@ -79,11 +81,11 @@ class CharactersViewController: UIViewController {
             }
         } failure: { error in
             print("Error")
-        }
+        }*/
     }
     
     @IBAction func backButtonAction(_ sender: Any) {
-        NetworkApi.shared.pages(url: (model.info?.prev)!) { allCharacters in
+     /*   NetworkApi.shared.pages(url: (model.info?.prev)!) { allCharacters in
             self.model = allCharacters
             self.collectionCharacters.reloadData()
             self.countPage -= 1
@@ -94,24 +96,22 @@ class CharactersViewController: UIViewController {
             }
         } failure: { error in
             print("Error")
-        }
+        }*/
     }
-    
 }
-
+    // MARK: - Extension de delegado -
 extension CharactersViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
+        ///TODO: puede que este id de error, hay que revisarlo
         NetworkApi.shared.getCharacter(id: model.results![indexPath.row].id) { character in
             let detailedView = DetailViewController(model: character)
             self.navigationController?.show(detailedView,
                                             sender: nil)
-        } failure: { error in
-            print("Error")
         }
     }
 }
 
+// MARK: - Extension de datasource -
 extension CharactersViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return model.results!.count
@@ -129,11 +129,11 @@ extension CharactersViewController: UICollectionViewDataSource {
         cell.statusView.backgroundColor = model.results![indexPath.row].statusColor()
         cell.statusView.layer.cornerRadius = 6
         cell.characterStatus.textColor = .black
-        
         return cell
     }
 }
 
+// MARK: - Extension de tabBar-
 extension CharactersViewController: UITabBarDelegate {
     func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
         if item.title == "Characters"{
@@ -141,34 +141,32 @@ extension CharactersViewController: UITabBarDelegate {
                 let myView = CharactersViewController(allCharacters)
                 self.navigationController?.pushViewController(myView,
                                                               animated: true)
-            } failure: { error in
-                print("Error")
-            }
+            } 
         } else if item.title == "Search" {
-            NetworkApi.shared.getAllCharacters { allCharacters in
+           /* NetworkApi.shared.getAllCharacters { allCharacters in
                 let myView = SearchViewController(allCharacters)
                 self.navigationController?.pushViewController(myView,
                                                               animated: true)
             } failure: { error in
                 print("Error")
-            }
+            }*/
         } else if item.title == "Episodes" {
-            NetworkApi.shared.getArrayEpisodes(season: "1,2,3,4,5,6,7,8,9,10,11") { episodes in
+         /*   NetworkApi.shared.getArrayEpisodes(season: "1,2,3,4,5,6,7,8,9,10,11") { episodes in
                 let myView = EpisodesViewController(episodes)
                 self.navigationController?.pushViewController(myView,
                                                               animated: true)
             } failure: { error in
                 print("Error")
-            }
+            }*/
 
         } else if item.title == "Locations" {
-            NetworkApi.shared.getAllLocations() { locations in
+        /*    NetworkApi.shared.getAllLocations() { locations in
                 let myView = LocationViewController( locations)
                 self.navigationController?.pushViewController(myView,
                                                               animated: true)
             } failure: { error in
                 print("Error")
-            }
+            }*/
         }
        
     }
